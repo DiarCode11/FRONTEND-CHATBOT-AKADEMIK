86b4fd8b268991c4435a5a0129a089fcbab0cf09
Diar Sudiar <163659290+DiarCode11@users.noreply.github.com>
2025-01-26 21:44:43 +0800

    style: modify table style

--- src/views/Auth/DashboardView.vue
+++ src/views/Auth/DashboardView.vue
@@ -1,70 +1,151 @@
 <template>
-    <p class="text-sm text-gray-400">Admin /</p>
-    <h1 class="text-xl font-bold pb-6">Dashboard</h1>
-    <div class="grid md:grid-cols-4 gap-4 mb-5 grid-cols-2">
-        <div class="bg-gradient-to-br shadow-xl from-sky-500 to-sky-600 text-white p-4 rounded-xl ">
-            <div class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl">
-                <i class="bi bi-person-circle text-gray-800 "></i>
-            </div>
-            <h1 class="pt-4 text-lg font-bold">800</h1>
-            <p>User Online</p>
-        </div>
-        <div class="bg-gradient-to-br shadow-xl from-green-500 to-green-600 text-white p-4 rounded-xl ">
-            <div class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl">
-                <i class="bi bi-chat-dots-fill text-gray-800"></i>
-            </div>
-            <h1 class="pt-4 text-lg font-bold">1000</h1>
-            <p>Chat Hari ini</p>
-        </div>
-        <div class="bg-gradient-to-br shadow-xl from-orange-500 to-orange-600 text-white p-4 rounded-xl ">
-            <div class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl">
-                <i class="bi bi-chat-right-heart-fill text-gray-800 translate-y-0.5"></i>
-            </div>
-            <h1 class="pt-4 text-lg font-bold">50</h1>
-            <p>Feedback</p>
-        </div>
-        <div class="bg-gradient-to-br shadow-xl from-slate-500 to-slate-600 text-white p-4 rounded-xl ">
-            <div class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl">
-                <i class="bi bi-hand-thumbs-up-fill text-gray-800"></i>
-            </div>
-            <h1 class="pt-4 text-lg font-bold">100</h1>
-            <p>Like</p>
-        </div>
-        
+  <p class="text-sm text-gray-400">Admin /</p>
+  <h1 class="text-xl font-bold pb-6">Dashboard</h1>
+  <div class="grid md:grid-cols-4 gap-4 mb-5 grid-cols-2">
+    <div
+      class="bg-gradient-to-br shadow-xl from-sky-500 to-sky-600 text-white p-4 rounded-xl"
+    >
+      <div
+        class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl"
+      >
+        <i class="bi bi-person-circle text-gray-800"></i>
+      </div>
+      <h1 class="pt-4 text-lg font-bold">800</h1>
+      <p>User Online</p>
     </div>
-    <div class="flex gap-4 bg-white rounded-3xl shadow-lg flex-col ">
-        <h1 class="text-lg font-semibold pt-4 pl-4">Log Aktivitas</h1>
-        <div class="overflow-x-auto w-full">
-            <div class="flex flex-col gap-2 w-max md:w-full h-max px-4 pb-10 rounded-3xl shadow-lg">
-                <table class="min-w-full w-max bg-white">
-                    <thead>
-                        <tr class="bg-gray-200 text-gray-600 uppercase text-xs">
-                            <th class="py-3 px-6 text-left">IP Address</th>
-                            <th class="py-3 px-6 text-left">Endpoint</th>
-                            <th class="py-3 px-6 text-left">Status</th>
-                            <th class="py-3 px-6 text-left">Timestamp</th>
-                            <th class="py-3 px-6 text-left">Description</th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-gray-600 text-xs">
-                        <tr v-for="i in 10" :key="i" class=" border-b">
-                            <td class="py-3 px-6 text-left">192.168.1.1</td>
-                            <td class="py-3 px-6 text-left">/admin</td>
-                            <td class="py-3 px-6 text-left">200</td>
-                            <td class="py-3 px-6 text-left">02-03-2023 12:00</td>
-                            <td class="py-3 px-6 text-left">OK</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>  
+    <div
+      class="bg-gradient-to-br shadow-xl from-green-500 to-green-600 text-white p-4 rounded-xl"
+    >
+      <div
+        class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl"
+      >
+        <i class="bi bi-chat-dots-fill text-gray-800"></i>
+      </div>
+      <h1 class="pt-4 text-lg font-bold">1000</h1>
+      <p>Chat Hari ini</p>
     </div>
+    <div
+      class="bg-gradient-to-br shadow-xl from-orange-500 to-orange-600 text-white p-4 rounded-xl"
+    >
+      <div
+        class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl"
+      >
+        <i class="bi bi-chat-right-heart-fill text-gray-800 translate-y-0.5"></i>
+      </div>
+      <h1 class="pt-4 text-lg font-bold">50</h1>
+      <p>Feedback</p>
+    </div>
+    <div
+      class="bg-gradient-to-br shadow-xl from-slate-500 to-slate-600 text-white p-4 rounded-xl"
+    >
+      <div
+        class="bg-white w-8 h-8 flex justify-center items-center rounded-full shadow-xl"
+      >
+        <i class="bi bi-hand-thumbs-up-fill text-gray-800"></i>
+      </div>
+      <h1 class="pt-4 text-lg font-bold">100</h1>
+      <p>Like</p>
+    </div>
+  </div>
+  <div class="relative flex gap-4 bg-white rounded-3xl shadow-lg flex-col">
+		<span class="bg-gradient-to-r from-white to-transparent w-4 h-full absolute my-10 left-0"></span>
+		<span class="bg-gradient-to-r from-transparent to-white w-4 h-full absolute my-10 right-0"></span>
+    <div class="w-full flex justify-between items-center px-6 pb-2 pt-7 gap-3">
+      <div class="">
+        <button
+          @click=""
+          class="w-9 h-9 md:w-full md:h-full md:px-3 md:py-1 justify-center items-center bg-sky-500 text-white rounded-lg flex gap-2 hover:scale-105 transition-all duration-200 ease-in-out"
+        >
+          <i class="bi bi-file-earmark-plus"></i>
+          <p class="md:block hidden">Tambah</p>
+        </button>
+      </div>
+      <div class="relative">
+        <input
+          type="text"
+          placeholder="Search..."
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none block w-full pl-10 p-2"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="absolute left-3 top-2.5 w-5 h-5 text-gray-400"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+      </div>
+    </div>
+    <div class="pb-10">
+      <div class="overflow-x-auto w-full">
+        <div class="flex flex-col gap-2 w-max md:w-full h-max px-4 shadow-lg">
+          <table class="min-w-full w-max bg-white">
+            <thead>
+              <tr class="bg-gray-200 text-gray-600 uppercase text-xs">
+                <th class="py-3 px-6 text-left">Deskripsi</th>
+                <th class="py-3 px-6 text-left">Nama File</th>
+                <th class="py-3 px-6 text-left">Timestamp</th>
+                <th class="py-3 px-6 text-left">Aksi</th>
+              </tr>
+            </thead>
+            <tbody class="text-gray-600 text-xs">
+              <tr v-for="i in 7" :key="i" class="border-b">
+                <td class="py-3 px-6 text-left">{{ 1 }}</td>
+                <td class="py-3 px-6 text-left">{{ 1 }}</td>
+                <td class="py-3 px-6 text-left">{{ 1 }}</td>
+                <td class="py-3 px-6 text-left flex gap-2">
+                  <EditButton @button-clicked="setDataEdited(file)" />
+                  <DeleteButton @button-clicked="setDataDeleted(file)" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <!-- Paginasi -->
+      <div class="flex justify-center gap-2 mt-4">
+        <button
+          @click="changePage(current_page - 1)"
+          :disabled="current_page === 1"
+          class="bg-gray-200 w-8 h-8 py-1 text-sm rounded-md"
+        >
+          <<
+        </button>
+        <button
+          v-for="page in visiblePages"
+          :key="page"
+          @click="changePage(page)"
+          :class="{
+            'bg-blue-500 text-white': current_page === page,
+            'cursor-default': page === '...',
+          }"
+          :disabled="page === '...'"
+          class="w-8 h-8 rounded-md"
+        >
+          {{ page }}
+        </button>
+        <button
+          @click="changePage(current_page + 1)"
+          :disabled="current_page === totalPages"
+          class="bg-gray-200 p-2 w-8 h-8 py-1 rounded-md"
+        >
+          >>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'DashboardView'
-}
+  name: "DashboardView",
+};
 </script>
 
 <style scoped>
@@ -73,7 +154,8 @@ table {
   border: none; /* Menghilangkan garis pada tabel */
 }
 
-table th, table td {
+table th,
+table td {
   border: none; /* Menghilangkan garis pada header dan sel */
   padding: 12px; /* Memberikan padding untuk ruang antar konten */
 }
