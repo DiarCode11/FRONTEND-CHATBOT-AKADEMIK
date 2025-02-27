d48e07f04a76f8ecf3b8d47272229264fdc4562f
Diar Sudiar <163659290+DiarCode11@users.noreply.github.com>
2025-02-27 19:12:30 +0800

    feat: add command for preview

--- src/views/Auth/AdminView.vue
+++ src/views/Auth/AdminView.vue
@@ -79,13 +79,12 @@
       <div class="fixed z-10 bottom-0 left-0 w-full bg-sky-500 h-10  md:hidden">
         <div class="w-full bg-white h-5"></div>
       </div>
-      <nav class="fixed z-20 bottom-0 left-0 w-full h-10 grid grid-cols-4 md:hidden transition-all duration-500 ease-in-out">
+      <nav class="fixed z-20 bottom-0 left-0 w-full h-10 grid grid-cols-3 md:hidden transition-all duration-500 ease-in-out">
         <router-link to="/admin" class="text-xl flex justify-center items-center"
           :class="{ 
             'text-sky-500 bg-white rounded-br-2xl': isNavActive('/admin'), 
             'text-white bg-sky-500 rounded-tr-2xl': $route.path.startsWith('/admin/dataset-management'), 
-            'text-white bg-sky-500 ': isNavActive('/admin/history-chat-process'),
-            'text-white bg-sky-500': isNavActive('/admin/feedback'),
+            'text-white bg-sky-500 ': isNavActive('/admin/history-chat-process')
           }"
         >
           <i class="bi bi-speedometer2 transition-all duration-300 ease-out"
@@ -99,7 +98,6 @@
             'text-white bg-sky-500 rounded-tl-2xl ': isNavActive('/admin'), 
             'text-sky-500 bg-white rounded-b-2xl ': $route.path.startsWith('/admin/dataset-management'), 
             'text-white bg-sky-500 rounded-tr-2xl ': isNavActive('/admin/history-chat-process'),
-            'text-white bg-sky-500 ': isNavActive('/admin/feedback'),
           }"
         >
           <i class="bi bi-database transition-all duration-300 ease-out"
@@ -112,27 +110,12 @@
           :class="{ 
             'text-white bg-sky-500': isNavActive('/admin'), 
             'text-white bg-sky-500 rounded-tl-2xl ': $route.path.startsWith('/admin/dataset-management'), 
-            'text-sky-500 bg-white rounded-b-2xl ': isNavActive('/admin/history-chat-process'),
-            'text-white bg-sky-500 rounded-tr-2xl': isNavActive('/admin/feedback'),
+            'text-sky-500 bg-white rounded-bl-2xl ': isNavActive('/admin/history-chat-process')
           }"
         >
           <i class="bi bi-chat-left-dots transition-all duration-300 ease-out"
             :class="{
               'text-3xl': isNavActive('/admin/history-chat-process')}"
-          ></i>
-        </router-link>
-        <router-link to="/admin/feedback" class="text-xl flex justify-center items-center"
-          :class="{ 
-            'text-white bg-sky-500 ': isNavActive('/admin'), 
-            'text-white bg-sky-500': $route.path.startsWith('/admin/dataset-management'), 
-            'text-white bg-sky-500 rounded-tl-2xl ': isNavActive('/admin/history-chat-process'),
-            'text-sky-500 bg-white rounded-bl-2xl': isNavActive('/admin/feedback'),
-          }"
-        >
-          <i class="bi bi-chat-right-heart transition-all duration-300 ease-out"
-            :class="{
-              'text-3xl': isNavActive('/admin/feedback')
-            }"
           ></i>
         </router-link>
       </nav>
--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,11 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 5002,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5002
   },
   build: {
     terserOptions: {
